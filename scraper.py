1de172ba6a8c634633d44499891a5e0ffa042b71
SAYAN DAS <poke19962008@gmail.com>
2017-03-02 01:23:20 +0530

    added script extracter

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup as bs
-import urllib2, pickle
+import urllib2, pickle, re
 
 class Scrape:
 
     def __init__(self):
         self.url = "http://www.imsdb.com/TV/Seinfeld.html"
         self.root = "http://www.imsdb.com"
-        self.extractLinks()
+
+        # self.extractLinks()
+        self.scrapeLinks()
 
     def extractLinks(self):
         with open("cache/%s" % self.url.split('/')[-1], "r") as f:
@@ -26,6 +28,39 @@ class Scrape:
         with open("cache/links.pkl", "wb") as f:
             pickle.dump(series, f)
             del series, soup
+
+    def scrapeLinks(self):
+        with open("cache/links.pkl", "rb") as f:
+            series =  pickle.load(f)
+            for season in series:
+                for ep in season:
+                    self.scrapePage(ep)
+                    break
+                break
+
+
+    def scrapePage(self, link):
+        link = urllib2.quote(link).replace("http%3A", "http:")
+        html = urllib2.urlopen(link).read()
+        soup = bs(html)
+
+        reg = re.compile(r'Read \"[\s\S]*\" Script')
+        elements = [e for e in soup.find_all('a') if reg.match(e.text)][0]
+        self.extractScript(self.root+urllib2.quote(elements['href']))
+
+
+    def extractScript(self, link):
+        html = urllib2.urlopen(link).read()
+        soup = bs(html)
+
+        pre = soup.find('td', class_="scrtext").find('pre')
+        counter = 0
+        for c in pre.strings:
+            c = re.sub(r'(\s\s+)|(\n)', ' ', c)
+
+            if re.match(r'\s[A-Z]+\s$|\s[A-Z]+\sAND\s[A-Z]+\s$', c):
+                print c
+            # print len(c), "++", c, "--"
 
 
     def cache(self):
