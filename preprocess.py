2124ec6fdff39680b23c0f9b9e0e542b53e595b3
SAYAN DAS <poke19962008@gmail.com>
2017-03-14 23:31:48 +0530

    added tokenizer

--- preprocess.py
+++ preprocess.py
@@ -8,13 +8,14 @@ class Preprocess:
         for i in xrange(len(script)):
             script[i][0] = self.cleanName(script[i][0])
             script[i][1] = self.cleanDial(script[i][1])
+            script[i][1] = self.tokenizeQuote(script[i][1])
 
+            # Check for 'jerry and john' and split it 2 unique quotes
             if re.match(r'(.+) and (.*)', script[i][0]):
                 actors = script[i][0].split(" and ")
 
                 script[i][0] = actors[0]
                 script.insert(i, [actors[1], script[i][1]])
-
         fname = fname.split('.')
         fname.insert(1, 'clean')
         fname = ".".join(fname)
@@ -22,23 +23,28 @@ class Preprocess:
         del script
         print "[SUCCESS] Cleaned", fname
 
-        
     def cleanName(self, name):
         return name.strip().lower()
 
     def cleanDial(self, d):
-        d = d.strip().lower()
-        d = re.sub(r'(\.\.+)|"|:|;', ' ', d)
-        d = re.sub(r'\s*\([^)]*\)', '', d)
-        d = re.sub(r'\s+', ' ', d)
-        return d.strip()
+        if not isinstance(d, list):
+            d = d.strip().lower()
+            d = re.sub(r'(\.\.+)|"|:|;', ' ', d) # Remove Multiple Spaces & Special ch.
+            d = re.sub(r'\s*\([^)]*\)', '', d) # Remove bracket and it's content
+            d = re.sub(r'\s+', ' ', d) # Remove Multiple Spaces
+            return d.strip()
+        else:
+            return d
 
     def cleanAllFiles(self):
         for f in os.listdir('data'):
             self.cleanFile(os.path.join("data", f))
 
-    def tokenizeFile(self, fname):
-        pass
+    def tokenizeQuote(self, quote):
+        if not isinstance(quote, list):
+            return nltk.word_tokenize(quote)
+        else:
+            return quote
 
 if __name__ == '__main__':
     Preprocess().cleanAllFiles()
